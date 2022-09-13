74105f4139b85e3be6a5ed6f8fee25e69837bde2
vnSasa <ol.yaremechko@gmail.com>
2022-09-13 10:51:56 +0300

    added functionality to connect to the http server and functionality to write data to the database through a form on the site

--- main.go
+++ main.go
@@ -4,45 +4,69 @@ import (
   "fmt"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
+  "net/http"
+  "html/template"
 )
 
-type User struct {
-  Email string `json : "email"`
-  FirstName string `json : "first_name"`
-  LastName string `json : "last_name"`
-  Password string `json : "password"`
-  CPassword string `json : "confirm_password"`
-}
+var database *sql.DB
 
 func main()  {
+  handleFunc()
+}
+
+func handleFunc()  {
   db, err := sql.Open("mysql", "sasa:110513@tcp(127.0.0.1:3306)/go-periodicals")
   if err != nil {
     panic(err)
   }
+  database = db
   defer db.Close()
 
-  // insert, err := db.Query("INSERT INTO users (email, first_name, last_name, password, confirm_password)" +
-  //                 " VALUES('sasa.yar@gmail.com', 'Olexandr', 'Yaremechko', '110513', '110513')")
-  // if err != nil {
-  //   panic(err)
-  // }
-  // defer insert.Close()
+  http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
+  http.HandleFunc("/", index)
+  http.HandleFunc("/create", create)
+  http.HandleFunc("/save_product", save_product)
 
-  res, err := db.Query("SELECT email, first_name, last_name, password, confirm_password FROM users")
+  fmt.Println("Server is listening...")
+  http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+  t, err := template.ParseFiles("templates/index.html", "templates/header.html",
+                                "templates/footer.html")
   if err != nil {
-    panic(err)
+    fmt.Fprintf(w, err.Error())
   }
+  t.ExecuteTemplate(w, "index", nil)
+}
 
-  for res.Next() {
-    var user User
-    err = res.Scan(&user.Email, &user.FirstName, &user.LastName, &user.Password, &user.CPassword)
+func create(w http.ResponseWriter, r *http.Request) {
+  t, err := template.ParseFiles("templates/create.html", "templates/header.html",
+                                "templates/footer.html")
+    if err != nil {
+      fmt.Fprintf(w, err.Error())
+    }
+    t.ExecuteTemplate(w, "create", nil)
+}
+
+func save_product(w http.ResponseWriter, r *http.Request) {
+
+  if r.Method == "POST" {
+    err := r.ParseForm()
+      if err != nil {
+      panic(err)
+    }
+
+    description := r.FormValue("description")
+    name_product := r.FormValue("name_product")
+    price := r.FormValue("price")
+
+    _, err = database.Exec("INSERT INTO products (description, name_product, price) " +
+                        "VALUES (?, ?, ?)", description, name_product, price)
     if err != nil {
       panic(err)
     }
 
-    fmt.Printf("User: email - %s, first_name - %s, last_name - %s, password - %s, confirm_password - %s\n",
-                user.Email, user.FirstName, user.LastName, user.Password, user.CPassword)
+    http.Redirect(w, r, "/", http.StatusSeeOther)
   }
-
-  fmt.Println("Work!!!")
 }
